40d89ef34cd9f47ea5be93c5448ad1e22eebafe5
sprudello <110892985+sprudello@users.noreply.github.com>
2024-10-31 18:20:58 +0100

    Created Base for Backend
    Edited Databasemodel and created the Tables for the Database

---
+++ octo-lounge-accountant-api/Models/Account.cs
@@ -0,0 +1,10 @@
+﻿namespace octo_lounge_accountant_api.Models
+{
+    public class Account
+    {
+        public int Id { get; set; }
+        public required string Name { get; set; }
+        public required int Type { get; set; }
+        public required int OwnerId { get; set; }
+    }
+}
---
+++ octo-lounge-accountant-api/Models/Records.cs
@@ -0,0 +1,14 @@
+﻿namespace octo_lounge_accountant_api.Models
+{
+    public class Records
+    {
+        public int Id { get; set; }
+        public required decimal Amount { get; set; }
+        public required string Description { get; set; }
+        public required int CreditorId { get; set; }
+        public required int DebtorId { get; set; }
+        public DateTime CreatedAt { get; set; } = DateTime.Now;
+        
+
+    }
+}
